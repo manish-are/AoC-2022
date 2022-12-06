45c195169d8ccf3170f191d97ae345a1cfa92f88
manish-are <manisharebsnl@gmail.com>
2022-12-06 16:09:26 +0530

    Aoc-Day6 added

---
+++ Day6/Day6/Program.cs
@@ -0,0 +1,28 @@
+﻿int marker = 0;
+string msg = System.IO.File.ReadAllText(args[0]);
+List<char> chars = new List<char>();
+foreach(var m in msg)
+{
+    if (chars.Contains(m))
+    {
+        chars.RemoveRange(0,chars.IndexOf(chars.Where(x => x == m).Last())+1);
+        chars.Add(m);
+        marker++;
+    }
+    else
+    {
+        if (chars.Count() < 14)
+        {
+            chars.Add(m);
+            marker++;
+            if (chars.Count() == 14) { break; }
+        }
+    }   
+}
+//4 incaseof 14 for part1
+Console.WriteLine(marker);
+
+
+
+
+
