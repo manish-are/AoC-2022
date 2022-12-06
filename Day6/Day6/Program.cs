5d7efb5d71838c830630f0088d0938de8f43a01a
manish-are <manisharebsnl@gmail.com>
2022-12-06 17:29:56 +0530

    AoC-Day6 added some elegant solutions from reddit.Have to analyse them

--- Day6/Day6/Program.cs
+++ Day6/Day6/Program.cs
@@ -24,5 +24,28 @@ Console.WriteLine(marker);
 
 
 
+//Func<int, int> getFirstDistinctChunk = chunkSize => new[] { File.ReadAllText("Input.txt") }
+//    .Select(s =>
+//        Enumerable.Range(0, s.Length - (chunkSize + 1))
+//        .Select(i => (i + chunkSize, s.Substring(i, chunkSize)))
+//        .First(x => x.Item2.Distinct().Count() == chunkSize)
+//        .Item1)
+//    .First();
 
+//Console.WriteLine($"Part one: {getFirstDistinctChunk(4)}");
+//Console.WriteLine($"Part two: {getFirstDistinctChunk(14)}");
+
+
+//input.TakeWhile((c, i) => i <= 4 || input.Skip(i-4).Take(4).Distinct().Count() < 4).Count()
+
+//chars.Select((x, i) => (chars.Skip(i).Take(4).Distinct().Count() == 4, i + 4)).First(x => x.Item1).Item2
+
+////    for (int i = 4; i < input.Length; i++)
+//{
+//    if (input.Skip(i - 4).Take(4).Distinct().Count() == 4)
+//    {
+//        Console.WriteLine(i);
+//        break;
+//    }
+//}
 
