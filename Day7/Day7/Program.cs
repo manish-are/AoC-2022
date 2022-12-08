e3b7466ee94f4a577e3eeb176d3c675e43a5439c
manish-are <manisharebsnl@gmail.com>
2022-12-08 10:09:09 +0530

    Added missing parts

--- Day7/Day7/Program.cs
+++ Day7/Day7/Program.cs
@@ -1,55 +1,123 @@
-﻿var folders = new List<string>();
-
-folders.Add("/");
-
-
-var root = new Tree();
+﻿var root = new Tree();
+var pointer = root;
 foreach (var line in System.IO.File.ReadLines(args[0]))
 {
-    var command = line.Split(' ');
-    if (command[0] == "dir")
+    var items = line.Split(" ");
+    //command
+    if (items[0] == "$")
     {
-        
-        root.AddFolder(command[1], );
+        if (items[1] == "cd")
+        {
+            if (items[2] == "/")
+            {
+                pointer = root;
+            }
+            else if (items[2] == "..")
+            {
+                pointer = pointer.Parent;
+            }
+            else
+            {
+                pointer = pointer.Folders[items[2]];
+            }
+        }
+        else if (items[1] == "ls")
+        {
+            continue;
+        }
     }
     else
     {
-        root.AddFile(command[1], int.Parse(command[0]));
-
+        if (items[0] == "dir")
+        {
+            pointer.AddFolders(items[1], new Tree(), pointer);
+        }
+        else
+        {
+            pointer.AddFiles(items[1], int.Parse(items[0]));
+        }
     }
-    
 }
 
 
 
+root.Print();
 
-
-
-
+Console.WriteLine(Tree.Sum);
+root.FindSize();
+Console.WriteLine(root.Size);
+Console.WriteLine(Tree.reqSizes.Min());
 
 
 public class Tree
 {
+    public static int Sum { get; set; } = 0;
 
-    public List<Dictionary<string, int>> Files { get; set; }
-
-    public List<Dictionary<string, Tree>> Folders { get; set; }
-
-
-    public void AddFile(string filename,int size)
+    public static List<int> reqSizes { get; set; } = new List<int>();
+    public Tree()
     {
-        var file = new Dictionary<string, int>();
-        file.Add(filename, size);
-        Files.Add(new Dictionary<string, int>(file));
+        Files = new Dictionary<string, int>();
+        Folders = new Dictionary<string, Tree>();
+    }
+    public Dictionary<string, int> Files { get; set; }
+
+    public Dictionary<string, Tree> Folders { get; set; }
+
+    public int Size { get; set; } = 0;
+
+    public Tree? Parent { get; set; }
+
+    public void AddFiles(string fileName, int size)
+    {
+        Files.Add(fileName, size);
+    }
+
+    public void AddFolders(string folderName, Tree child, Tree parent)
+    {
+        Folders.Add(folderName, child);
+        child.Parent = parent;
+    }
+
+    public void FindSize()
+    {
+        Size = 0;
+        foreach (var file in Files)
+        {
+            Size += file.Value;
+        }
+
+
+        foreach (var folder in Folders)
+        {
+            folder.Value.FindSize();
+            Size += folder.Value.Size;
+        }
+
 
     }
 
-    public void AddFolder(string folderName, Tree folder)
-    {
-        var data = new Dictionary<string, Tree>();
-        data.Add(folderName, folder);
-        Folders.Add(new Dictionary<string, Tree>(data));
-    }
 
+    public void Print()
+    {
+        foreach (var file in Files)
+        {
+            Console.WriteLine(file);
+        }
+        foreach (var folder in Folders)
+        {
+            Console.WriteLine(folder);
+            folder.Value.FindSize();
+            Console.WriteLine(folder.Value.Size);
+            if (folder.Value.Size >= 528671)
+            {
+                reqSizes.Add(folder.Value.Size);
+            }
+            //par1
+            //Sum += folder.Value.Size <= 100000 ? folder.Value.Size : 0;
+            folder.Value.Print();
+
+        }
+
+    }
 }
 
